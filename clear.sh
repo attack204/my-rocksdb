ebc04c0dc6ab49db004d0efe2f26c84ecc00e8ef
attack204 <attack204@qq.com>
2022-11-25 12:45:14 +0800

    update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,6 +1,6 @@
-rm level.out
-rm lifetime.out
-rm number_life.out
-rm factor.out
-rm last_compact.out
-rm rank.out
+rm -f level.out 
+rm -f lifetime.out 
+rm -f number_life.out 
+rm -f factor.out 
+rm -f last_compact.out
+rm -f rank.out 
