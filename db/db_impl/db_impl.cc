0c73d45f2703b651d6750165896e9de332cc8f9a
attack204 <attack204@qq.com>
2022-12-01 16:32:57 +0800

    update

--- db/db_impl/db_impl.cc
+++ db/db_impl/db_impl.cc
@@ -5894,11 +5894,11 @@ void add_level_file_num(int level) {
   level_file_num[level]++;
 }
 int get_ave_time(int level) {
-  if(!compacted_number[level]) return 0;
+  if(compacted_number[level] <= 0) return 0;
   return compact_level_lifetime[level] / compacted_number[level];
 }
 double get_predict_rate(int level) {
-  if(!compacted_number[level]) return 0;
+  if(compacted_number[level] <= 0) return 0;
   return (double) correct_predict_time[level] / compacted_number[level];
 }
 int min_int(int a, int b) {
