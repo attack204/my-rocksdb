5b804a2554c2f093e0789f0b6352c8d95ba943a3
attack204 <attack204@qq.com>
2022-12-09 19:20:09 +0800

    add feature in real_lifetime.py

--- real_lifetime.py
+++ real_lifetime.py
@@ -50,15 +50,27 @@ for line in open("lifetime.out"):
     real_type.append(int(line.split(' ')[4]))
 
 for l in range(0, 6): #each level
-    real_lifetime_list = []
+    real_lifetime_list_0 = []
+    real_lifetime_list_n1 = []
     sum = 0
     tot = 0
+    cnt0 = 0
+    cntn1 = 0
     for i in range(0, len(x_list)):
         if x_list[i] == l:
-            real_lifetime_list.append(lifetime_list[i])
+            if real_type[i] == 0:
+                real_lifetime_list_0.append(lifetime_list[i])
+                cnt0 += 1
+            else:
+                real_lifetime_list_n1.append(lifetime_list[i])
+                cntn1 += 1
             sum += lifetime_list[i]
             tot += 1
-    print("level=%d average lifetime=%d" % (l, sum / tot))
-    if(len(real_lifetime_list) != 0):
-        plt.hist(real_lifetime_list, bins=20, color="yellow")
+    print("level=%d average lifetime=%d cnt0=%d cntn1=%d" % (l, sum / tot, cnt0, cntn1))
+    if(len(real_lifetime_list_0) != 0):
+        plt.hist(real_lifetime_list_0, bins=20, color="yellow")
         plt.show()
+    if(len(real_lifetime_list_n1) != 0):
+        plt.hist(real_lifetime_list_n1, bins=20, color="red")
+        plt.show()
+
